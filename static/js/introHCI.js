047bf5aa13da5fc67ee9d4b7ae924b18e011ee8b
Joey Dang <jodang@ucsd.edu>
2021-01-19 19:08:39 -0800

    Finish lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,29 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("JavaScript has taken control.");
+		$(".jumbotron p").addClass("active");
+		$("#testjs").text("Please wait...");
+    //$('.jumbotron h1').text("Javascript is connected");
+    $("#testjs").toggleClass("active");
+    $("a.thumbnail").click(projectClick);
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) {
+  // prevent the page from reloading   
+  e.preventDefault();
+  // In an event handler, $(this) refers to   
+  // the object that triggered the event zz   
+  $(this).css("background-color", "#7fff00");
+  var containingProject = $(this).closest(".project");
+  var description = $(containingProject).find(".project-description");
+  if (description.length == 0) {
+    $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+  } else {
+    description.toggle()
+  }
 }
